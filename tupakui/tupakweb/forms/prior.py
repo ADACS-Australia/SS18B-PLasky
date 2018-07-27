e31d8393181c011e05234d9de9f8eca96e840973
macrocosme <danyvohl@hotmail.com>
2018-07-27 14:14:20 +1000

    Create prior form

---
+++ tupakui/tupakweb/forms/prior.py
@@ -0,0 +1,118 @@
+"""
+Distributed under the MIT License. See LICENSE.txt for more info.
+"""
+
+from django import forms
+
+# GLOBAL DECLARATIONS
+RADIO = 'radio'
+SELECT = 'select'
+TEXT = 'text'
+NUMBER = 'number'
+DEFAULT_INITIAL = 'Fixed'
+DEFAULT_CHOICES = (
+    ('Fixed', 'Fixed'),
+    ('Uniform', 'Uniform'),
+)
+
+def get_radio_input(label, choices=None, initial=None):
+    if not choices:
+        choices = DEFAULT_CHOICES
+        initial = DEFAULT_INITIAL
+
+    return forms.ChoiceField(
+        label=label,
+        widget=forms.RadioSelect,
+        choices=choices,
+        initial=initial,
+    )
+
+
+def get_text_input(label, placeholder=None, initial=None):
+    return forms.CharField(
+        label=label,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': placeholder,
+            }
+        ),
+        required=False,
+        initial=initial,
+    )
+
+
+def get_number_input(label):
+    return forms.CharField(
+        label=label,
+        widget=forms.NumberInput(
+            attrs={
+                'class': 'form-control',
+            }
+        ),
+        required=False,
+    )
+
+
+def get_select_input(label, choices=None, initial=None):
+    if not choices:
+        choices = DEFAULT_CHOICES
+        initial = DEFAULT_INITIAL
+
+    return forms.ChoiceField(
+        label=label,
+        widget=forms.Select(
+            attrs={
+                'class': 'form-control',
+            }
+        ),
+        choices=choices,
+        initial=initial,
+
+    )
+
+class Prior(forms.Form):
+    """Class to represent a Prior. It can be any of the following types:
+    1. Radio (Currently in use)
+    2. Select
+    3. Text (Currently in use)
+    4. Number
+
+    """
+    field_types = [
+        RADIO,
+        SELECT,
+        TEXT,
+        NUMBER,
+    ]
+
+    field_type = field_types[1]
+
+    def __init__(self, name, label, choices=None, initial=None, field_type='', placeholder='', *args, **kwargs):
+        super(Prior, self).__init__(*args, **kwargs)
+
+        self.field_type = field_type if self.field_types.__contains__(field_type) else self.field_type
+
+        # adding custom fields
+        if self.field_type == RADIO:
+            self.fields[name] = get_radio_input(
+                label=label,
+                choices=choices,
+                initial=initial
+            )
+        elif self.field_type == SELECT:
+            self.fields[name] = get_select_input(
+                label=label,
+                choices=choices,
+                initial=initial,
+            )
+        elif self.field_type == TEXT:
+            self.fields[name] = get_text_input(
+                label=label,
+                placeholder=placeholder,
+                initial=initial,
+            )
+        elif self.field_type == NUMBER:
+            self.fields[name] = get_number_input(
+                label=label,
+            )
