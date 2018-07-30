0de256e682b6230e0e53f7eada6b61af17b2d0a6
Shibli Saleheen <ssaleheen@swin.edu.au>
2018-07-30 17:44:27 +1000

    change template for password reset completed

--- tupakui/accounts/urls.py
+++ tupakui/accounts/urls.py
@@ -23,13 +23,16 @@ urlpatterns = [
          ), name='password_reset'),
     path('password_reset/done/',
          auth_views.PasswordResetDoneView.as_view(
-            template_name='accounts/registration/password_reset_done.html',
-         ), name='password_reset_done',),
+             template_name='accounts/registration/password_reset_done.html',
+         ), name='password_reset_done', ),
     path('reset/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(
              form_class=PasswordResetCustomForm,
              template_name='accounts/registration/password_reset_confirm.html',
          ), name='password_reset_confirm'),
-    path('reset/done', auth_views.password_reset_complete, name='password_reset_complete'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(
+            template_name='accounts/registration/password_reset_complete.html',
+         ), name='password_reset_complete'),
     path('verify/', verify, name='verify_account'),
 ]
