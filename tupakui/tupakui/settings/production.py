5a26e5cf079332c039f9131b0951f2ed277d2c15
Shibli Saleheen <ssaleheen@swin.edu.au>
2018-07-31 17:29:18 +1000

    change of timezone and Jobs in the admin

--- tupakui/tupakui/settings/production.py
+++ tupakui/tupakui/settings/production.py
@@ -8,6 +8,7 @@ CSRF_COOKIE_SECURE = False
 SESSION_COOKIE_SECURE = False
 CONN_MAX_AGE = 900  # 15 minutes of persistent connection
 
+EMAIL_FROM = ''
 EMAIL_HOST = 'mail.swin.edu.au'
 EMAIL_PORT = 25
 
