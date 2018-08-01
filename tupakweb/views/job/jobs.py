3aa3752f297045184f4617d6d47597d5c051e60a
Shibli Saleheen <ssaleheen@swin.edu.au>
2018-08-01 13:48:14 +1000

    job list initial

--- tupakweb/views/job/jobs.py
+++ tupakweb/views/job/jobs.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+
 from ...models import Job
+
 
 @login_required
 def jobs(request):
-    my_jobs = Job.objects.filter(user=request.user)
+    my_jobs = Job.objects.filter(Q(user=request.user), ~Q(status__in=[Job.DELETED, ]))
 
     return render(
         request,
