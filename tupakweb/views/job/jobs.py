0823baeb65fb43a4f65c1c080a57e78223f1e35f
Shibli Saleheen <ssaleheen@swin.edu.au>
2018-08-03 18:31:01 +1000

    job list

--- tupakweb/views/job/jobs.py
+++ tupakweb/views/job/jobs.py
@@ -1,18 +1,24 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from ...models import Job
 
 
 @login_required
 def jobs(request):
-    my_jobs = Job.objects.filter(Q(user=request.user), ~Q(status__in=[Job.DELETED, ]))
+    my_jobs = Job.objects.filter(Q(user=request.user), ~Q(status__in=[Job.DELETED, ])).order_by('-submission_time',
+                                                                                                '-creation_time')
+    paginator = Paginator(my_jobs, 5)
+
+    page = request.GET.get('page')
+    job_list = paginator.get_page(page)
 
     return render(
         request,
         "tupakweb/job/all-jobs.html",
         {
-            'job': my_jobs,
+            'jobs': job_list,
         }
     )
