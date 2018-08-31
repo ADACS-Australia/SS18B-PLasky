0305c80277b3e8b7b9293a23fade87d7860ac50a
Shibli Saleheen <ssaleheen@swin.edu.au>
2018-08-31 18:46:03 +1000

    prior fields

--- tupakweb/forms/prior/utility.py
+++ tupakweb/forms/prior/utility.py
@@ -29,6 +29,26 @@ def prior_fixed_field(field_name, field_value):
     return name + '_fixed', value
 
 
+def prior_min_field(field_name, field_value):
+    name = field_name
+    value = field_value.copy()
+
+    value.update({
+        'label': 'Min',
+    })
+    return name + '_min', value
+
+
+def prior_max_field(field_name, field_value):
+    name = field_name
+    value = field_value.copy()
+
+    value.update({
+        'label': 'Max',
+    })
+    return name + '_max', value
+
+
 def get_field_properties_by_signal_choice(signal_choice):
     field_properties = OrderedDict()
     if signal_choice == Signal.BINARY_BLACK_HOLE:
@@ -44,4 +64,17 @@ def get_field_properties_by_signal_choice(signal_choice):
             field_properties.update({
                 name_for_fixed: value_for_fixed,
             })
+
+            # setting up the min value field
+            name_for_min, value_for_min = prior_min_field(name, value)
+            field_properties.update({
+                name_for_min: value_for_min,
+            })
+
+            # setting up the max value field
+            name_for_max, value_for_max = prior_max_field(name, value)
+            field_properties.update({
+                name_for_max: value_for_max,
+            })
+
         return field_properties
